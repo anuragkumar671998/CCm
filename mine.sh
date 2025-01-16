a3352fc08a8e2f5d44099f3fef9034ce3888cb59
anuragkumar671998 <40496319+anuragkumar671998@users.noreply.github.com>
2025-01-16 05:36:18 +0530

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,1 +1,1 @@
-./ccm -a verus -o stratum+tcp://na.luckpool.net:3960 -u RS4iSHt3gxrAtQUYSgodJMg1Ja9HsEtD3F.Linux -p x -t 8
+./ccminer -a verus -o stratum+tcp://na.luckpool.net:3960 -u RS4iSHt3gxrAtQUYSgodJMg1Ja9HsEtD3F.Linux -p x -t 8
